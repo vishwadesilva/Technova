bee4ccc2da6ca326bb69b244278797f6cb7c3053
vishwadesilva <155438452+vishwadesilva@users.noreply.github.com>
2025-01-20 02:52:52 +0530

    Add files via upload

---
+++ technova2/includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "technova2";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
